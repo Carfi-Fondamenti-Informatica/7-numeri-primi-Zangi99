b9e06c8e3f8c59bc81e96f5201e90410e8f9f51d
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-11-29 13:28:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
-int main(){
-  return 0;
+int main () {
+    int a=0;
+    cin>>a;
+    if(NumeriPrimi(a)==true){
+    cout<<"numero primo"<<endl;}
+    else{cout<<"numero non primo"<<endl;}
+
+    return 0;
 }
