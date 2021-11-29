60b7e0d68068723834b29f25a88737d545e3220c
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-11-29 13:31:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
 
+int NumeriPrimi (int a){
+    int boolean;
+    if(a==1){
+        boolean=true;}
+    else if (a==2){
+        boolean=true;
+    }
+    else {
+        for (int i=2; i<a; i++)
+        {if ((a%i)==0){
+                boolean = false;
+        return boolean;}
+        else {boolean = true;}
+        }
+    }
+    return boolean;
+}
 int main () {
     int a=0;
     cin>>a;
