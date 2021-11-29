9978b80a336358b6c533ae4ba2503b660427de85
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-11-29 13:28:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,17 @@
-
+int NumeriPrimi (int a){
+    int boolean;
+    if(a==1){
+        boolean=true;}
+    else if (a==2){
+        boolean=true;
+    }
+    else {
+        for (int i=2; i<a; i++)
+        {if ((a%i)==0){
+                boolean = false;
+        return boolean;}
+        else {boolean = true;}
+        }
+    }
+    return boolean;
+}
